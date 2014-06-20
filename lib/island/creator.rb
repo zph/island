9d8c197b827b2e54ebfa4a45d129573237caed3c
Zander Hill <Zander@civet.ws>
2014-06-19 23:26:18 -0400

    Because friends don\'t let friends name objects after data strucutres. Thx @adkron

--- lib/island/creator.rb
+++ lib/island/creator.rb
@@ -1,36 +1,37 @@
 module Island
 
   class Creator
-    attr_accessor :files, :content
-    def self.call(files, opts={})
-      files = Set.new(files)
-      rejects = opts.fetch(:rejects)    { Plugins::REJECTIONS }
-      mods = opts.fetch(:modifications) { Plugins::MODIFICATIONS }
-      creator = new(files)
-      files_hash = creator.read_content
-      files_hash = creator.alter_each_file(files_hash)
-      ## process each file
-      content = creator.blend_files(files_hash)
-      r = creator.requires(content)
+    attr_accessor :files
 
-      creator.ensure_dependencies(r, files)
+    def self.call(files, opts={})
+      files                = Set.new(files)
+      rejects              = opts.fetch(:rejects)    { Plugins::REJECTIONS }
+      mods                 = opts.fetch(:modifications) { Plugins::MODIFICATIONS }
+      creator              = new(files)
+      files_hash           = creator.read_content
+      files_hash           = creator.alter_each_file(files_hash)
+      ## process each file
+      content              = creator.blend_files(files_hash)
+      requires             = creator.requires(content)
+
+      creator.ensure_dependencies(requires, files)
 
       content = creator.reject_line(content, rejects)
       content = creator.modify_lines(content, mods)
       creator.join_lines(content)
     end
 
-    def alter_each_file(hash)
+    def alter_each_file(files_with_content)
       # Act on the array of lines of each file
       # ie prepend ==Start of filename / ==End of filename
       # h.map do |k,v|
       # end
 
-      hash
+      files_with_content
     end
 
-    def blend_files(hash)
-      @content = hash.values.flatten
+    def blend_files(files_with_content)
+      files_with_content.values.flatten
     end
 
     def ensure_dependencies(libs, files)
@@ -47,9 +48,9 @@ module Island
 
     def requires(content)
       reqs = content.select { |i| i[/^\s?require/]}
-                 .map { |o| o[/['"](.*)['"]/]; $1 }[0..6]
-      hash = reqs.map { |o| stdlib?(o) }.flatten
-      Hash[*hash]
+                    .map { |o| o[/['"](.*)['"]/]; match = $1 }[0..6]
+      files_and_content = reqs.map { |o| stdlib?(o) }.flatten
+      Hash[*files_and_content]
     end
 
     def stdlib?(lib)
@@ -70,25 +71,25 @@ module Island
       @files = files
     end
 
-    def read_content(f = files)
-      Hash[*f.flat_map { |z| [z, File.open(z).readlines.map(&:chomp)] }]
+    def read_content(filenames = files)
+      Hash[*filenames.flat_map { |z| [z, File.open(z).readlines.map(&:chomp)] }]
     end
 
-    def reject_line(c, plugins = Plugins::REJECTIONS)
+    def reject_line(content, plugins = Plugins::REJECTIONS)
       # plugins must eval to true for it to be removed
-      plugins.each_with_object(c) do |b, obj|
+      plugins.each_with_object(content) do |b, obj|
         obj.reject!{ |l| b.call(l) }
       end
     end
 
-    def modify_lines(c, plugins = Plugins::MODIFICATIONS)
-      plugins.each_with_object(c) do |b, obj|
+    def modify_lines(content, plugins = Plugins::MODIFICATIONS)
+      plugins.each_with_object(content) do |b, obj|
         b.call(obj)
       end
     end
 
-    def join_lines(c = content)
-      c.join("\n") + "\n"
+    def join_lines(content)
+      content.join("\n") + "\n"
     end
   end
 end
